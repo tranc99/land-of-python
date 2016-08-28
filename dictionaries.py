cb208cda711c011f6dcdf950da9a041df2a347ed
tomahawk <tmutunhire@gmail.com>
2016-08-28 16:54:16 +0200

    dictionaries

--- dictionaries.py
+++ dictionaries.py
@@ -7,3 +7,16 @@ ab = {
 
 print("Swaroop's address is ", ab["Swaroop"])
 print("Matsumoto's address is", ab["Matsumoto"])
+
+del(ab["Spammer"])
+
+print("\nThere are {} contacts in the address-book\n".format(len(ab)))
+
+for name, address in ab.items():
+    print("Contact {} at {} ".format(name, address))
+
+ab["Guido"] = "guido@python-land.org"
+
+if "Guido" in ab:
+    print("\nGuido's address is: ", ab["Guido"])
+    print("\nThere are now {} contacts in the address book".format(len(ab)))
